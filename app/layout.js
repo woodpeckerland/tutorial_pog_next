8b651623b1c729eb4b1da33f6190151c7f0c1ad6
woodpecker <hans.peter.specht@gmail.com>
2025-06-26 12:24:36 +0200

    Tailwind

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+import Menu from "@/app/components/Menu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Link href="/"> Home </Link>
-        <Link href="/about"> About </Link>
-        <Link href="/contact"> Contact </Link>
-
+        <Menu />
         {children}
       </body>
     </html>
