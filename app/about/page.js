d91636aa99504d8ea55d467864f1d80247d11b7f
woodpecker <hans.peter.specht@gmail.com>
2025-06-26 09:43:32 +0200

    Init, Routing

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Hier gibt es mehr Info!</p>
+    </div>
+  );
+}
---
+++ app/contact/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>Kontakt</h1>
+      <p>Adresse, Telefon, E-Mail etc.</p>
+    </div>
+  );
+}
